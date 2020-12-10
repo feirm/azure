744758e1c85272ff715adae51a1ddd3cbed54b24
Jack Coble <jack@coble.uk>
2020-12-10 14:32:10 +0000

    Remove another redundant function

--- pkg/coins/coin.go
+++ pkg/coins/coin.go
@@ -1,20 +1,5 @@
 package coins
 
-import (
-	"errors"
-	"strings"
-)
-
 var Coins = map[string]*Coin{
 	"XFE": &Feirm,
-}
-
-// Check if a coin exists and return its data
-func FetchCoin(ticker string) (*Coin, error) {
-	coin, ok := Coins[strings.ToUpper(ticker)]
-	if !ok {
-		return nil, errors.New("Coin does not exist.")
-	}
-
-	return coin, nil
 }
