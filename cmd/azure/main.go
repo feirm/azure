5fc0877e4785c6c120e0cfcd6a5aca7c6b9193b4
Jack Coble <jack@coble.uk>
2020-12-10 01:44:53 +0000

    Start HTTP server on port 8080

--- cmd/azure/main.go
+++ cmd/azure/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "log"
+import (
+	"log"
+	"net/http"
+
+	"github.com/feirm/azure/internal/controller"
+)
 
 func main() {
 	log.Println("Azure microservice...")
+
+	// Routes
+	http.HandleFunc("/v1/coins", controller.ListAllCoins)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
