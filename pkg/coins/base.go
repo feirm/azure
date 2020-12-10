246500d76b1ddc8a13ecf6f66f6415f5d2e6cf75
Jack Coble <jack@coble.uk>
2020-12-10 01:05:10 +0000

    Update coins

--- pkg/coins/base.go
+++ pkg/coins/base.go
@@ -1,22 +1,12 @@
 package coins
 
-import (
-	"github.com/btcsuite/btcd/chaincfg"
-)
-
 // Representation of a full "coin"
 type Coin struct {
-	Information CoinInformation `json:"coinInformation"`
-	NetParams   *chaincfg.Params
-}
-
-type CoinInformation struct {
 	Name      string                 `json:"name"`
 	Ticker    string                 `json:"ticker"`
-	Icon      string                 `json:"icon"` // Base-64 encoded PNG
+	Icon      string                 `json:"icon"`
 	Blockbook string                 `json:"blockbook"`
 	TxVersion int                    `json:"txVersion"`
-	TxBuilder string                 `json:"txBuilder"`
 	BIP44     int                    `json:"bip44"`
 	Networks  map[string]CoinNetwork `json:"networks"`
 }
